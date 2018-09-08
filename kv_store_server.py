74654f5248f8e87797b517eaecd397422934b300
Andrew Dollard <andrewdollard@gmail.com>
2018-09-08 11:41:25 -0700

    storing to disk, badly

--- kv_store_server.py
+++ kv_store_server.py
@@ -1,16 +1,34 @@
 import cmd
+import os
 
 class KVCli(cmd.Cmd):
 
-    data = {}
+    def load_data(self):
+        data = {}
+        if not os.path.exists('data_file'):
+            return {}
+
+        with open('data_file', 'r+') as data_file:
+            for line in data_file.readlines():
+                key, value = line.rstrip().split('=')
+                data[key] = value
+        return data
+
+    def store_data(self, data):
+        with open('data_file', 'w+') as data_file:
+            for key in data:
+                data_file.write('{}={}\n'.format(key, data[key]))
 
     def do_set(self, pair):
+        data = self.load_data()
         key, value = pair.split('=')
-        self.data[key] = value
+        data[key] = value
+        self.store_data(data)
 
     def do_get(self, key):
-        if key in self.data:
-            print(self.data[key])
+        data = self.load_data()
+        if key in data:
+            print(data[key])
         else:
             print('not found')
 
