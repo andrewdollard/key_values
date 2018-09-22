a4368f20edd2dc35522275b9bf109f18629ba35e
Andrew Dollard <andrewdollard@gmail.com>
2018-09-22 13:36:59 -0700

    client cleanup

--- client.py
+++ client.py
@@ -1,55 +1,43 @@
 import cmd
 import constants
 import socket
+import random
 from serialization import serialize_set, serialize_get
 from net import receive
 
-def open_socket(port=1234):
-    s = socket.socket(socket.AF_INET)
-    # try:
-    s.connect(('localhost', port))
-    # except:
-    #     s = socket.socket(socket.AF_INET)
-    #     s.connect(('localhost', 1235))
+def make_request(msg):
+    response = b''
+    port = random.choice([1234, 1235, 1236])
+    count = 0
 
-    return s
+    while True:
+        count += 1
+        if count > 3:
+            break
+
+        s = socket.socket(socket.AF_INET)
+        s.connect(('localhost', port))
+        s.send(msg)
+        response = receive(s)
+        s.close()
+
+        if response[0:1] != constants.FORWARD:
+            break
+        port = int.from_bytes(response[1:], byteorder='big')
+    return response
 
 class KVCli(cmd.Cmd):
 
     def do_set(self, pair):
         key, value = pair.split('=')
         payload = serialize_set(key, value)
-
-        s = open_socket()
-        s.send(payload)
-        response = receive(s)
+        response = make_request(payload)
         print(response)
-        s.close()
-        if response[0:1] == constants.FORWARD:
-            port = int.from_bytes(response[1:], byteorder='big')
-            s = open_socket(port)
-            s.send(payload)
-            response = receive(s)
-            print(response)
-            s.close()
-
 
     def do_get(self, key):
         payload = serialize_get(key)
-
-        s = open_socket()
-        s.send(payload)
-        response = receive(s)
+        response = make_request(payload)
         print(response)
-        s.close()
-
-        if response[0:1] == constants.FORWARD:
-            port = int.from_bytes(response[1:], byteorder='big')
-            s = open_socket(port)
-            s.send(payload)
-            response = receive(s)
-            print(response)
-            s.close()
 
         if response[0:1] == constants.GET_OK:
             print(response[1:])
