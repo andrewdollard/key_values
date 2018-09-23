449cd528ccde40d0ea7cec62fb866df29735767c
Andrew Dollard <andrewdollard@gmail.com>
2018-09-22 18:13:51 -0700

    fix port reclaiming

--- cluster.sh
+++ cluster.sh
@@ -4,14 +4,13 @@ if [[ $1 == '-d' ]]; then
 	rm -rf data
 fi
 
-netstat -vanp tcp | grep -E '1234|1235|1236' | awk '{print $9}' | xargs sudo kill -9
-
 cleanup() {
   kill `jobs -p` 2>/dev/null
 }
 trap "cleanup; exit 0" INT TERM EXIT
 
 for f in {1234..1236}; do
+  netstat -vanp tcp | grep -E "$f" | awk '{print $9}' | xargs sudo kill -9
 	python3 -u server.py "$f" | awk '{ print "'"$f"': "$0 }' &
 done
 
