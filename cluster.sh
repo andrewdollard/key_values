98f34b4486bc5dfe624ca6c77ca8dfe2fa6ce0fd
Andrew Dollard <andrewdollard@gmail.com>
2018-09-22 19:11:46 -0700

    finally got process kill working

--- cluster.sh
+++ cluster.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-if [[ $1 == '-d' ]]; then
-	rm -rf data
-fi
-
 cleanup() {
   kill `jobs -p` 2>/dev/null
 }
 trap "cleanup; exit 0" INT TERM EXIT
 
-for f in {1234..1236}; do
-  netstat -vanp tcp | grep -E "$f" | awk '{print $9}' | xargs sudo kill -9
+if [[ $1 == '-d' ]]; then
+	rm -rf data
+fi
+
+for pid in $(netstat -vanp tcp | grep -E '1234|1235|1236'| awk '{print $9}'); do
+  kill -9 $pid
+done
+
+for f in 1234 1235 1236; do
 	python3 -u server.py "$f" | awk '{ print "'"$f"': "$0 }' &
 done
+
+python3 -u seed.py | awk '{ print "seed: "$0 }' &
 
 wait
 cleanup
